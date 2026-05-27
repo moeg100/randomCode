97a2b736a2a74c24e9bc64c70995715764c395d6
moeg100 <moeg@umich.edu>
2026-05-27 07:51:32 -0400

    Update r1.py
    Fixing the name in factorial and implementing fibonacci.

--- r1.py
+++ r1.py
@@ -1,3 +1,4 @@
+#Factorial in recursive
 
 def factorial(a):
     if a == 1 or a == 0:
@@ -5,4 +6,21 @@ def factorial(a):
     return a * factorial(a-1)
     
     
-print(factorials(5))
+print(factorial(5))
+
+
+
+
+#Fibonacci in recursive
+def fibo(n):
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+        
+    return fibo(n-1) + fibo(n-2)
+    
+    
+    
+for i in range(30):
+    print(fibo(i))
