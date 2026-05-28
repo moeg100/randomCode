8be270f63dbdf1b328c20eee474d4bc06b9513f4
moeg100 <moeg@umich.edu>
2026-05-27 23:57:14 -0400

    Adding the print loop that prints the characters in reverse order.

--- warm2.py
+++ warm2.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 import requests as rq
 from pprint import pprint
 
-html_doc = rq.get("https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub")
+#html_doc = rq.get("https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub")
 #print(html_doc.text)
-
+html_doc = rq.get("https://docs.google.com/document/d/e/2PACX-1vSvM5gDlNvt7npYHhp_XfsJvuntUhq184By5xO_pA4b_gCWeXb6dM6ZxwN8rE6S4ghUsCj2VKR21oEP/pub")
 soup = BeautifulSoup(html_doc.text, 'html.parser')
 
 
@@ -24,14 +24,14 @@ for row in table.find_all("tr")[1:]:
 	#print(f"VALUES : {x}, {char}, {y} ") 
 	values[(int(x), int(y))] = char
 
-print(values)
+#print(values)
 #print(a.find_all("td")[0].get_text().strip())
 
 max_x = max([x for x, y in values.keys()])
 max_y = max([y for x, y in values.keys()])
 
-print(max_x)
-print(max_y)
+#print(max_x)
+#print(max_y)
 
 grid = []
 
@@ -49,7 +49,17 @@ for (x, y), char in values.items():
 	grid[x][y] = char
 	
 	
-print(grid)
+#print(grid)
+
+for r in range(max_y, -1, -1):
+	print("".join(grid[r]))
+
+
+
+
+
+
+
 
 """
 for value in values:
