bf41b0fe6703d274b48297d99192376e439d70e7
moeg100 <moeg@umich.edu>
2026-07-17 23:08:21 -0400

    Refactor game rendering and player controls
    Updated player movement and rendering logic, changed background color, and modified rectangle positions.

--- pyGameBasic.py
+++ pyGameBasic.py
@@ -10,21 +10,43 @@ size = w, h, = 500, 300
 
 screen = pygame.display.set_mode(size)
 
-rect = pygame.draw.rect(screen, "Green", (100, 100, 100, 100), 6)
+clock = pygame.time.Clock()
 
+dt = 0
+
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
     
-    screen.fill((255, 255, 255))
-    pygame.draw.rect(screen, (231, 55, 21), (100, 150, 200, 100))
-    target_rect = pygame.Rect(400, 150, 200, 100)
-    pygame.draw.rect(screen, (66, 111, 232), target_rect, width=5)
-    
-    pygame.display.flip()
+    screen.fill((12, 32, 55))
 
+
+    # Render start here
+
+    pygame.draw.rect(screen, (231, 55, 21), (200, 150, 200, 100))
+    target_rect = pygame.Rect(200, 50, 200, 100)
+    pygame.draw.rect(screen, (66, 111, 232), target_rect, width=22)
+
+    pygame.draw.circle(screen, "green", player_pos, 40)
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+
+    pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 
 
