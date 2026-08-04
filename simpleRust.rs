f8df427b497d2b2e833dec55db45b5a9c4194110
moeg100 <moeg@umich.edu>
2026-08-03 23:50:36 -0400

    Refactor simpleRust.rs for input validation and logic
    Updated the random range for variable 'a' and refactored the answer assignment using a more concise if-else expression. Modified the main function to return an ExitCode and added input validation.

--- simpleRust.rs
+++ simpleRust.rs
@@ -1,35 +1,42 @@
 use std::io;
+use std::process::ExitCode;
+
 
 fn simple_s(input:String)-> String{
-    let a = rand::random_range(1..3);
+    let a = rand::random_range(1..4);
     println!("Input is : {input} and a is {a}");
     
-    let mut answer;
-    if a == 1 {
-        answer = "Rock";
-    }
-    else if a == 2 {
-        answer = "Paper";
-        
-    }
-    else {
-        answer = "Scissor";
-    }
+    let answer = if a == 1 {
+        "Rock"
+    } else if a == 2 {
+        "Paper"
+    } else {
+        "Scissor"
+    };
+    
     println!("ANSWER IS : {answer}");
     return input
 }
 
 
-fn main() {
+fn main() -> ExitCode {
 
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(n) => {
-            //println!("{n} bytes read");
-            //println!("{input}");
-            simple_s(input);
+        Ok(_) => {
+            let cleaned_input = input.trim().to_lowercase();
+            
+            if !(cleaned_input == "rock" || cleaned_input == "paper" || cleaned_input == "scissor") {
+                println!("Input doesn't match the game words");
+                return ExitCode::from(42);
+            }
+            simple_s(cleaned_input);
+            ExitCode::SUCCESS
         }
-        Err(error) => println!("error: {error}"),
+        Err(error) => {
+            println!("error: {error}");
+            ExitCode::FAILURE
+            }
+        }
     }
  
-}
