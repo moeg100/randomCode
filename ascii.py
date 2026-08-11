7c01407ce8bb7e5ff0bf56deebbf1b848f9bf965
moeg100 <moeg@umich.edu>
2026-08-10 23:11:51 -0400

    Add new ASCII text and modify display logic
    Added a new ASCII text variable and updated the print logic to display it alongside the existing ASCII text.

--- ascii.py
+++ ascii.py
@@ -28,14 +28,19 @@ ascii_text = r"""
 ╚███╔███╔╝╚██████╔╝██║  ██║███████╗██████╔╝
  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝
 """
-
+ascii_text_2 = r"""
+This is a text
+"""
 i = 0
 
 try:
     while True:
         os.system("cls" if os.name == "nt" else "clear")
         print(colors[i % len(colors)] + ascii_text + RESET)
+        time.sleep(2.15)
+        print(colors[i % len(colors)] + ascii_text_2 + RESET)
+        time.sleep(1.0)
         i += 1
-        time.sleep(0.15)
+
 except KeyboardInterrupt:
     print(RESET)
