a4e9f52ef17b43fe9a85a8644f69fe0bc1be6e2c
moeg100 <moeg@umich.edu>
2026-08-25 23:13:09 -0400

    Enhance ASCII art with dynamic fonts and colors
    Updated color range and added font cycling for ASCII art.

--- ascii.py
+++ ascii.py
@@ -2,12 +2,12 @@ import time
 import os
 import pyfiglet
 import random
-
+from pyfiglet import FigletFont
 # ANSI color codes
 colors = []
 
 for i in range(random.randint(1,10)):
-    colorNumber = random.randint(30, 99)
+    colorNumber = random.randint(80, 99)
     color = "\033[{}m".format(colorNumber)
     colors.append(color) 
 
@@ -17,13 +17,14 @@ RESET = "\033[0m"
 # Ask for text
 text = input("Enter text: ")
 
-# Convert input into ASCII art
-ascii_text = pyfiglet.figlet_format(text, font="big")
+
+fonts = FigletFont.getFonts()
 
 i = 0
 
 try:
     while True:
+        ascii_text = pyfiglet.figlet_format(text, font=fonts[i % len(fonts)])
         os.system("cls" if os.name == "nt" else "clear")
 
         print(colors[i % len(colors)] + ascii_text + RESET)
