d87ca0915119e492d082beb614cee43ea99639b2
moeg100 <moeg@umich.edu>
2026-08-27 22:29:30 -0400

    Modify random range for colors and iterations
    Increased the range of random iterations and adjusted color number range for better variety.

--- ascii.py
+++ ascii.py
@@ -6,8 +6,8 @@ from pyfiglet import FigletFont
 # ANSI color codes
 colors = []
 
-for i in range(random.randint(1,10)):
-    colorNumber = random.randint(80, 99)
+for i in range(random.randint(1,20)):
+    colorNumber = random.randint(40, 99)
     color = "\033[{}m".format(colorNumber)
     colors.append(color) 
 
@@ -17,7 +17,6 @@ RESET = "\033[0m"
 # Ask for text
 text = input("Enter text: ")
 
-
 fonts = FigletFont.getFonts()
 
 i = 0
@@ -25,6 +24,7 @@ i = 0
 try:
     while True:
         ascii_text = pyfiglet.figlet_format(text, font=fonts[i % len(fonts)])
+        
         os.system("cls" if os.name == "nt" else "clear")
 
         print(colors[i % len(colors)] + ascii_text + RESET)
