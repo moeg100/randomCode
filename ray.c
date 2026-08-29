d01a7ea27cb8373928e16cf4b981b7de6b612c3a
moeg100 <moeg@umich.edu>
2026-08-28 22:03:20 -0400

    Create ray.c

---
+++ ray.c
@@ -0,0 +1,147 @@
+#include "raylib.h"
+#include <stdio.h>
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Raylib Timer");
+    SetTargetFPS(60);
+
+    // Timer settings
+    float timer = 10.0f;
+    const float startTime = 10.0f;
+    bool running = false;
+
+    // Counter
+    int counter = 0;
+
+    // Timer colors
+    Color timerColors[] = {
+        WHITE,
+        SKYBLUE,
+        GREEN,
+        YELLOW,
+        ORANGE,
+        RED,
+        PINK
+    };
+
+    int colorIndex = 0;
+    int colorCount = sizeof(timerColors) / sizeof(timerColors[0]);
+
+    // Buttons
+    Rectangle startButton = { 100, 330, 140, 50 };
+    Rectangle resetButton = { 330, 330, 140, 50 };
+    Rectangle colorButton = { 560, 330, 140, 50 };
+
+    while (!WindowShouldClose())
+    {
+        // -------------------------
+        // UPDATE
+        // -------------------------
+
+        if (running)
+        {
+            timer -= GetFrameTime();
+
+            if (timer <= 0.0f)
+            {
+                timer = startTime;
+                counter++;
+            }
+        }
+
+        // Start / Pause button
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            Vector2 mouse = GetMousePosition();
+
+            if (CheckCollisionPointRec(mouse, startButton))
+            {
+                running = !running;
+            }
+
+            // Reset button
+            if (CheckCollisionPointRec(mouse, resetButton))
+            {
+                timer = startTime;
+                running = false;
+                counter = 0;
+            }
+
+            // Change color button
+            if (CheckCollisionPointRec(mouse, colorButton))
+            {
+                colorIndex++;
+
+                if (colorIndex >= colorCount)
+                {
+                    colorIndex = 0;
+                }
+            }
+        }
+
+        // -------------------------
+        // DRAW
+        // -------------------------
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        // Title
+        DrawText("SIMPLE RAYLIB TIMER", 250, 40, 30, RAYWHITE);
+
+        // Timer
+        int seconds = (int)timer;
+
+        char timerText[32];
+        sprintf(timerText, "%02d", seconds);
+
+        int textWidth = MeasureText(timerText, 100);
+
+        DrawText(
+            timerText,
+            screenWidth / 2 - textWidth / 2,
+            120,
+            100,
+            timerColors[colorIndex]
+        );
+
+        // Counter
+        char counterText[32];
+        sprintf(counterText, "Counter: %d", counter);
+
+        DrawText(
+            counterText,
+            screenWidth / 2 - MeasureText(counterText, 30) / 2,
+            250,
+            30,
+            WHITE
+        );
+
+        // Start / Pause button
+        DrawRectangleRec(startButton, DARKGRAY);
+
+        if (running)
+            DrawText("PAUSE", 135, 345, 20, WHITE);
+        else
+            DrawText("START", 135, 345, 20, WHITE);
+
+        // Reset button
+        DrawRectangleRec(resetButton, DARKGRAY);
+        DrawText("RESET", 365, 345, 20, WHITE);
+
+        // Color button
+        DrawRectangleRec(colorButton, DARKGRAY);
+        DrawText("COLOR", 595, 345, 20, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
